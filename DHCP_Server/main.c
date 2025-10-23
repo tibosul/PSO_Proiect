7c50648ceffd63a25f45252f29a546e899343903
Tiberiu Stelian <steliantiberiuandrei2005@gmail.com>
2025-10-23 18:30:51 +0300

    First commit

---
+++ DHCP_Server/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello from DHCP Server!\n");
+    return 0;
+}
---
+++ DNS_Server/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello from DNS Server!\n");
+    return 0;
+}
