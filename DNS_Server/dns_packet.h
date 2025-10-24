d528b9e40c707ffe89568b5b3d878ef68127a5d7
StefanMarina04 <stefan.marina69@gmail.com>
2025-10-24 13:26:08 +0300

    dns_packet initial

---
+++ DNS_Server/dns_packet.h
@@ -0,0 +1,26 @@
+#ifndef DNS_PACKET_H
+#define DNS_PACKET_H
+
+#include <stdint.h>
+#include <stdio.h>
+
+typedef struct {
+
+}dns_header ;
+
+typedef struct {
+
+}dns_question;
+
+typedef struct {
+
+}resource_record;
+
+#define CACHE_SIZE 32
+#define MAX_PACKET_SIZE 512
+
+typedef struct {
+
+}cache_entry;
+
+#endif 
