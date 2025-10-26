6bcbb4be240f32c7172c1bd6c628de44ff0ced39
andramoraru <andra.moraru15@gmail.com>
2025-10-26 22:57:34 +0200

    Files for configuration

--- DHCP_Server/DHCPv6/config_v6.h
+++ DHCP_Server/DHCPv6/config_v6.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 #include <stdbool.h>
-
+#include <netinet/in.h>
 
 
 #define HOSTNAME_MAX 256
@@ -17,15 +17,27 @@ typedef struct
     char hostname[HOSTNAME_MAX];
     char duid[DUID_MAX];
     char fixed_address6[Ip6_STR_MAX];
+
+    struct in6_addr fixed_addr6_bin;
+    bool has_fixed_address6_bin; //if the fixed adress was valid and converted
 }dhcpv6_static_host_t;
 
 typedef struct 
 {
+    //Subnet details
    char prefix[Ip6_STR_MAX];
    uint8_t prefix_len;
 
+   struct in6_addr prefix_bin;
+   bool has_prefix_bin;
+
    char pool_start[Ip6_STR_MAX];
    char pool_end[Ip6_STR_MAX];
+
+
+   struct in6_addr pool_start_bin;
+   struct in6_addr pool_end_bin;
+   bool has_pool_range; 
 
    char dns_servers[256];
    char domain_search[256];
@@ -42,12 +54,18 @@ typedef struct
    char pd_pool_start[Ip6_STR_MAX];
    char pd_pool_end[Ip6_STR_MAX];
    uint8_t pd_prefix_len;
+
+
+   struct in6_addr pd_pool_start_bin;
+   struct in6_addr pd_pool_end_bin;
+   bool has_pd_pool;
 }dhcpv6_subnet_t;
 
 
 typedef struct{
     uint32_t default_lease_time;
     uint32_t max_lease_time;
+
     char global_dns_servers[256];
     char global_domain_search[256];
 }dhcpv6_global_t;
@@ -55,6 +73,7 @@ typedef struct{
 
 typedef struct{
     dhcpv6_global_t global;
+
     dhcpv6_subnet_t subnets[MAX_SUBNET_V6];
     uint16_t subnet_count;
 }dhcpv6_config_t;
@@ -62,7 +81,9 @@ typedef struct{
 
 int load_config_v6(const char *path, dhcpv6_config_t *config);
 void dump_config_v6(const dhcpv6_config_t *config);
+dhcpv6_subnet_t *find_subnet_for_ipv6(const dhcpv6_config_t *cfg, const struct in6_addr *ip);
+dhcpv6_static_host_t *find_host_by_duid(dhcpv6_subnet_t *subnet,const char *duid);
 
-
+void convert_all_to_binary(dhcpv6_config_t *cfg);
 
 #endif
--- DHCP_Server/DHCPv6/utilsv6.h
+++ DHCP_Server/DHCPv6/utilsv6.h
@@ -5,7 +5,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <netinet/in.h>
-
+#include <stddef.h>
 
 bool is_valid_ipv6(const char *ip_str);
 int ipv6_str_to_bin(const char *ip_str, struct in6_addr *addr);
@@ -14,8 +14,9 @@ int ipv6_bin_to_str(const struct in6_addr *addr, char *ip_str, size_t size);
 int ipv6_compare(const struct in6_addr *addr1, const struct in6_addr *addr2);
 bool ipv6_in_subnet(const struct in6_addr *ip, const struct in6_addr *subnet, uint8_t prefix_len);
 
+bool ipv6_increment(struct in6_addr *ip);
 
-bool iv_valid_mac(const char *mac_str);
-bool iv_valid_duid(const char *duid_str);
+bool is_valid_mac(const char *mac_str);
+bool is_valid_duid(const char *duid_str);
 
 #endif
